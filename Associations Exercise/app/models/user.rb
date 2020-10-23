e26eba366d1aa190c58343273d6d4a9a440fc5b3
thedavidelrod <67294425+thedavidelrod@users.noreply.github.com>
2020-10-22 17:33:31 -0800

    Add files via upload

---
+++ Associations Exercise/app/models/user.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    validates :name, presence: true
+
+    has_many :enrollments,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+    
+    has_many :courses,
+        through: :enrollments,
+        source: :course
+
+end
